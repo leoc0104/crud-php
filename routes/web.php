783bd354f9f0538a71061840335aa2c13e5a4878
Leonardo Camargo <leocamargo0104@gmail.com>
2024-09-29 19:40:03 -0300

    feat(crud): Routes file

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,26 @@
 <?php
 
-require_once '../controllers/ProductController.php';
-require_once '../config/Database.php';
+use WordController\WordController;
+
+require_once '../Controllers/WordController.php';
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 
-$controller = new ProductController();
+$controller = new WordController();
 
-if ($uri[2] == 'products' && $_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($uri[3])) {
-        $controller->show($uri[3]); // GET /products/{id}
+if ($uri[1] == 'words' && $_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($uri[2])) {
+        $controller->show($uri[2]);
     } else {
-        $controller->index(); // GET /products
+        $controller->index();
     }
-} elseif ($uri[2] == 'products' && $_SERVER['REQUEST_METHOD'] == 'POST') {
-    $controller->store(); // POST /products
-} elseif ($uri[2] == 'products' && $_SERVER['REQUEST_METHOD'] == 'PUT') {
-    $controller->update($uri[3]); // PUT /products/{id}
-} elseif ($uri[2] == 'products' && $_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    $controller->destroy($uri[3]); // DELETE /products/{id}
-} elseif ($uri[2] == 'test' && $_SERVER['REQUEST_METHOD'] == 'GET') {
-    $database = new Database();
-    $connection = $database->getConnection();
-    
-    echo '<h1>Database Connection Test</h1>';
-    echo '<pre>';
-    var_dump($connection);
-    echo '</pre>';
+} elseif ($uri[1] == 'words' && $_SERVER['REQUEST_METHOD'] == 'POST') {
+    $controller->store();
+} elseif ($uri[1] == 'words' && $_SERVER['REQUEST_METHOD'] == 'PUT') {
+    $controller->update($uri[2]);
+} elseif ($uri[1] == 'words' && $_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    $controller->destroy($uri[2]);
 } else {
     header("HTTP/1.1 404 Not Found");
     echo json_encode(['message' => 'Route not found']);
